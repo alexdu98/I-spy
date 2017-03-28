32b1297ac62604f48c3ea1c79b8cb11196fa4d2a
Alexandre <alexdu98@gmx.fr>
2017-03-28 16:43:33 +0200

    Initial commit

---
+++ app/src/main/java/com/um/asn/i_spy/ChooseModeActivity.java
@@ -0,0 +1,13 @@
+package com.um.asn.i_spy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+    }
+}
