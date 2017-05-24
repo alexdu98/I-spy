e5cb26b4cfa36e8abaabe17e24fb1a929369f9f3
viampietro <vincent.iampietro@outlook.fr>
2017-05-24 10:33:04 +0200

    Ajout de la classe Config

---
+++ app/src/main/java/com/um/asn/i_spy/Config.java
@@ -0,0 +1,8 @@
+package com.um.asn.i_spy;
+
+public class Config {
+
+    public final static String SERVER_DOMAIN = "https://ispy.calyxe.fr/index.php/";
+    public final static String USER_INFO = "user_info";
+
+}
